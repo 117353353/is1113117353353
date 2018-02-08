6820626db8e0fd7afaa8dd7def0c5252b98f7749
Brian Hally <117353353@umail.ucc.ie>
2018-02-08 23:36:54 +0000

    Ebus

---
+++ ebusiness/Ebus2.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+    <html>     
+    <head>    
+        <title>Selected product</title>       
+        <!--jQuery-->        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>   
+        <script type="text/javascript" src="cost_calc.js"></script>  
+    </head>    
+    
+    <body>
+       <h4>  Please enter your payment details</h4>
+       
+       <br/>
+       
+       <form method="POST" action="Ebus3.php">
+           <label for="user_pin">PIN</label>
+           <input type="password" id="user_pin" placeholder="Card Pin" maxlength="4"
+           <button type="submit" id="btnPurchase"disabled>Proceed with Purchase</button>
+       </form>
+       
+       <br/>
+       
+       <button onClick="validateDetails()">Validate</button>
+       
+       <script type="text/javascript" src="Ebus2_validator.js"></script>
+       
+    </body>
+    <html/>
+    
+    
